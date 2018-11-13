b270a0dd1b1ffe0188733d42245aed944a30591e
Jesse Quinn <me@jessequinn.info>
2018-11-13 15:50:55 -0200

    added the full course

--- JavaPrograms/ByteShortInt/src/Main.java
+++ JavaPrograms/ByteShortInt/src/Main.java
@@ -2,6 +2,19 @@ package com.jessequinn;
 
 public class Main {
     public static void main(String[] arg) {
-        int myValue = 10000;
+
+        // int has a width of 32
+        int myMinValue = -2147483648;
+        int myMaxValue = 2147483647;
+
+        // byte has a width of 8
+        byte myByteValue = -128;
+
+        // short has a width of 18
+        short myShortValue = 32767;
+
+        // long has a width of 64
+        long myLongValue = 9_223_372_036_854_775_807L;
+
     }
 }
