d0cb51bfa02343edb66a38329baca5e50023c570
Jesse Quinn <me@jessequinn.info>
2018-11-28 21:18:03 -0200

    removed mp4, csv, and zip files

---
+++ Java Programs/GenericsChallenge/src/com/jessequinn/FootballPlayer.java
@@ -0,0 +1,8 @@
+package com.jessequinn;
+
+public class FootballPlayer extends Player {
+
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ Java Programs/GenericsChallenge/src/com/jessequinn/SoccerPlayer.java
@@ -0,0 +1,8 @@
+package com.jessequinn;
+
+public class SoccerPlayer extends Player {
+
+    public SoccerPlayer(String name) {
+        super(name);
+    }
+}
